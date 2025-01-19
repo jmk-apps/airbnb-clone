bfcf457645dd4585e6ca1522fbd39a086ba881af
John Kaganda <jkaganda@gmail.com>
2025-01-19 09:32:51 +0200

    Add Button and Modal components for enhanced user interaction

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body
         className={font.className}
       >
+        <Modal title="Hello World" isOpen />
         <Navbar />
         {children}
       </body>
