7e67652c80fb6007e0f9f8beafa9e2bc348523ad
John Kaganda <jkaganda@gmail.com>
2025-01-21 17:08:13 +0200

    Add registration modal enhancements with footer and toast notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body
         className={font.className}
       >
+        <ToasterProvider />
         <RegisterModal />
         <Navbar />
         {children}
