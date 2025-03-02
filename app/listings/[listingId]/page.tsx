355c6d43289c9c93aac96bb9a85b3fdda71c62f8
John Kaganda <jkaganda@gmail.com>
2025-03-02 10:30:37 +0200

    Add getReservations action and integrate it into ListingPage component

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -3,13 +3,15 @@ import getListingById from '@/app/actions/getListingById'
 import EmptyState from '@/app/components/EmptyState'
 import React from 'react'
 import ListingClient from './ListingClient'
+import getReservations from '@/app/actions/getReservations'
 
 interface IParams {
   listingId?: string
 }
 
 const ListingPage = async ({ params }: { params: IParams }) => {
-  const listing = await getListingById(params) 
+  const listing = await getListingById(params)
+  const reservations = await getReservations(params)
   const currentUser = await getCurrentUser()
   
   if (!listing) {
@@ -20,6 +22,7 @@ const ListingPage = async ({ params }: { params: IParams }) => {
     <div>
         <ListingClient 
           listing={listing}
+          reservations={reservations}
           currentUser={currentUser}
         />
     </div>
