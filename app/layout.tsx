9176cf4e6147f9cdedef817c68093bf51f46b134
John Kaganda <jkaganda@gmail.com>
2025-03-09 11:18:06 +0200

    Add search functionality with modal for filtering listings by location, date, and guest count. Update getListings action to support new filters.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import LoginModal from "./components/modals/LoginModal";
 import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
 import RentModal from "./components/modals/RentModal";
+import SearchModal from "./components/modals/SearchModal";
 
 
 const geistSans = Geist({
@@ -43,6 +44,7 @@ export default async function RootLayout({
       >
         <ToasterProvider />
         <LoginModal />
+        <SearchModal />
         <RentModal />
         <RegisterModal />
         <Navbar currentUser={currentUser} />
